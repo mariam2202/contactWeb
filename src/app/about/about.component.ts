522350b4681783bee4a9b445480ee35c62c063b4
mariam2202 <maroeloumian@gmail.com>
2020-06-22 15:44:57 +0200

    le composant about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  infos = {nom: 'Derruette', prenom: 'Elik', dateNaissance: '1960-11-25', email: 'maroaslan@hotmail.com', tel: 106578216, photo: './Image/IMG_2.jpg'};
 
   constructor() { }
 
